0cf12ba9fd955438d4432fe9bf46d4bb054c0b7a
Igor Kovalev <igor.alex.kovalev@gmail,com>
2023-09-08 09:34:59 +0300

    add homework 34

--- homework34_hibernate/src/main/java/org/example/servise/AbstractService.java
+++ homework34_hibernate/src/main/java/org/example/servise/AbstractService.java
@@ -1,21 +1,8 @@
 package org.example.servise;
 
-import org.example.crud.Person;
-import org.example.crud.TypeOfUser;
 import org.example.settings.AppSessionFactory;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-import org.hibernate.engine.spi.SessionLazyDelegator;
-
-import javax.persistence.*;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaDelete;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.CriteriaUpdate;
-import javax.persistence.metamodel.Metamodel;
-import java.io.Serializable;
-import java.util.List;
-import java.util.Map;
 
 public class AbstractService<T> {
     public T saveObject(T t) {
