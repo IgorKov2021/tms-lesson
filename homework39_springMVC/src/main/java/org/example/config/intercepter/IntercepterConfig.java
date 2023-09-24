555ffa5a5adb4eea544c339cef45b7c762b76602
Igor Kovalev <igor.alex.kovalev@gmail,com>
2023-09-24 17:16:46 +0300

    add homework 40

--- homework39_springMVC/src/main/java/org/example/config/intercepter/IntercepterConfig.java
+++ homework39_springMVC/src/main/java/org/example/config/intercepter/IntercepterConfig.java
@@ -5,12 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @Configuration
 @EnableWebMvc
 public class IntercepterConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        registry.addInterceptor(localeChangeInterceptor);
         registry.addInterceptor(new BookIntercepter());
+
     }
 }
